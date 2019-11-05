c024fc961115f123713fbdf67e3a68392dce9aad
JihyeKim0923 <sos13313@naver.com>
2019-11-05 15:08:26 +0900

    first commit

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post,Like,Comment, User
+from .models import Post,Like, User
 import operator
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
